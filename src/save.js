48c7d3f426e5462eac04042b542f3c7c5755e35d
Marko Krstic <mailme@markokrstic.com>
2024-01-11 22:35:44 +0100

    enabled wide and full on frontend

--- src/save.js
+++ src/save.js
@@ -2,15 +2,20 @@ import { extractYoutubeId, getPlaceholderImageUrl } from "./youtubeHelpers";
 import PlayContent from "./playContent";
 
 const Save = ({ attributes }) => {
-    const { url, containerId } = attributes;
+    const { url, containerId, align } = attributes;
     const youtubeId = extractYoutubeId(url);
-    const placeholderImageUrl = getPlaceholderImageUrl(youtubeId);
+
+    // Construct the class name based on the alignment
+    const className = `lazy-youtube-player-container ${
+        align ? "align" + align : ""
+    }`;
 
     return (
-        <div id={containerId} className="lazy-youtube-player-container">
+        <div id={containerId} className={className}>
             {youtubeId && <PlayContent url={url} />}
         </div>
     );
 };
 
 export default Save;
+
