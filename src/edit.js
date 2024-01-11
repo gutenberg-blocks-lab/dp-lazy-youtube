ae5a5e6c38348ef4ad03a10e888940fe3fcebb3d
Marko Krstic <mailme@markokrstic.com>
2024-01-11 16:54:02 +0100

    ...

--- src/edit.js
+++ src/edit.js
@@ -1,46 +1,11 @@
-// Edit.js
+import { __ } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
+import './editor.scss';
 
-import { useState } from "@wordpress/element";
-import { TextControl } from "@wordpress/components";
-
-const Edit = ({ attributes, setAttributes }) => {
-    const { url } = attributes;
-    const [youtubeId, setYoutubeId] = useState("");
-
-    const extractYoutubeId = (url) => {
-        const regex =
-            /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^\"&?\/ ]{11})/;
-        const matches = url.match(regex);
-        return matches ? matches[1] : "";
-    };
-
-    const handleUrlChange = (url) => {
-        setAttributes({ url });
-        setYoutubeId(extractYoutubeId(url));
-    };
-
-    const placeholderImageUrl = youtubeId
-        ? `https://img.youtube.com/vi/${youtubeId}/maxresdefault.jpg`
-        : "";
-
-    return (
-        <div>
-            <TextControl
-                label="YouTube URL"
-                value={url || ""}
-                onChange={handleUrlChange}
-                placeholder="Enter YouTube URL"
-            />
-            {youtubeId && (
-                <div className="youtube-preview">
-                    <img
-                        src={placeholderImageUrl}
-                        alt="YouTube Video Placeholder"
-                    />
-                </div>
-            )}
-        </div>
-    );
-};
-
-export default Edit;
+export default function Edit() {
+	return (
+		<p { ...useBlockProps() }>
+			{ __( 'Dp Init – hello from the editor!', 'dp-init' ) }
+		</p>
+	);
+}
--- src/save.js
+++ src/save.js
@@ -1,42 +1,9 @@
-const Save = ({ attributes }) => {
-    const { url } = attributes;
+import { useBlockProps } from '@wordpress/block-editor';
 
-    const extractYoutubeId = (url) => {
-        const regex =
-            /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^\"&?\/ ]{11})/;
-        const matches = url.match(regex);
-        return matches ? matches[1] : "";
-    };
-
-    const youtubeId = extractYoutubeId(url);
-    const placeholderImageUrl = youtubeId
-        ? `https://img.youtube.com/vi/${youtubeId}/maxresdefault.jpg`
-        : "";
-    const random_number = Math.floor(100000 + Math.random() * 900000);
-
-    return (
-        <div
-            id={`youtube-player-container-${random_number}`}
-            className="lazy-youtube-player-container"
-        >
-            {youtubeId && (
-                <>
-                    <button
-                        className="play-button"
-                        id={`play-button-${random_number}`}
-                        data-youtube-id={youtubeId}
-                    >
-                        {/* Put your play button SVG or icon here */}
-                    </button>
-                    <img
-                        src={placeholderImageUrl}
-                        alt="YouTube Video Placeholder"
-                        className="youtube-placeholder-image"
-                    />
-                </>
-            )}
-        </div>
-    );
-};
-
-export default Save;
+export default function save() {
+	return (
+		<p { ...useBlockProps.save() }>
+			{ 'Dp Init – hello from the saved content!' }
+		</p>
+	);
+}
