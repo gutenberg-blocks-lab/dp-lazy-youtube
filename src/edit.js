f704076f4e69c044e6c2ad7280a96e40a062e410
Marko Krstic <mailme@markokrstic.com>
2024-01-11 18:08:05 +0100

    ..

--- src/edit.js
+++ src/edit.js
@@ -1,14 +1,18 @@
 // Edit.js
 
 import { useState, useEffect } from "@wordpress/element";
-import { TextControl } from "@wordpress/components";
-import { useBlockProps } from "@wordpress/block-editor";
+import {
+    TextControl,
+    ToolbarGroup,
+    ToolbarButton,
+} from "@wordpress/components";
+import { BlockControls, useBlockProps } from "@wordpress/block-editor";
 
 const Edit = ({ attributes, setAttributes }) => {
     const { url } = attributes;
     const [youtubeId, setYoutubeId] = useState("");
+    const [isEditing, setIsEditing] = useState(false);
 
-    // Function to extract YouTube ID from the URL
     const extractYoutubeId = (url) => {
         const regex =
             /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^\"&?\/ ]{11})/;
@@ -16,38 +20,49 @@ const Edit = ({ attributes, setAttributes }) => {
         return matches ? matches[1] : "";
     };
 
-    // Effect to update the YouTube ID when the URL changes
     useEffect(() => {
         setYoutubeId(extractYoutubeId(url));
     }, [url]);
 
-    // Function to handle URL input changes
     const handleUrlChange = (newUrl) => {
         setAttributes({ url: newUrl });
     };
 
-    // URL for the YouTube thumbnail image
     const placeholderImageUrl = youtubeId
         ? `https://img.youtube.com/vi/${youtubeId}/maxresdefault.jpg`
         : "";
 
     return (
-        <div {...useBlockProps()}>
-            <TextControl
-                label="YouTube URL"
-                value={url || ""}
-                onChange={handleUrlChange}
-                placeholder="Enter YouTube URL"
-            />
-            {youtubeId && (
-                <div className="youtube-preview">
-                    <img
-                        src={placeholderImageUrl}
-                        alt="YouTube Video Placeholder"
+        <>
+            <BlockControls>
+                <ToolbarGroup>
+                    <ToolbarButton
+                        icon={isEditing ? "edit" : "visibility"}
+                        label={isEditing ? "Edit URL" : "View Image"}
+                        onClick={() => setIsEditing(!isEditing)}
                     />
-                </div>
-            )}
-        </div>
+                </ToolbarGroup>
+            </BlockControls>
+            <div {...useBlockProps()}>
+                {isEditing ? (
+                    <TextControl
+                        label="YouTube URL"
+                        value={url || ""}
+                        onChange={handleUrlChange}
+                        placeholder="Enter YouTube URL"
+                    />
+                ) : (
+                    youtubeId && (
+                        <div className="youtube-preview">
+                            <img
+                                src={placeholderImageUrl}
+                                alt="YouTube Video Placeholder"
+                            />
+                        </div>
+                    )
+                )}
+            </div>
+        </>
     );
 };
 
