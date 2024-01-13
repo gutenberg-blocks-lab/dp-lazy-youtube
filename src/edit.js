a306f60a5542ab9a7355485a299e3b2e629975e2
Marko Krstic <mailme@markokrstic.com>
2024-01-13 10:26:29 +0100

    1.0.1

--- src/edit.js
+++ src/edit.js
@@ -36,32 +36,42 @@ const Edit = ({ attributes, setAttributes }) => {
         </div>
     );
 
+    const renderToolbar = () => (
+        <BlockControls>
+            <ToolbarGroup>
+                <ToolbarButton
+                    icon={isEditing ? "edit" : "visibility"}
+                    label={isEditing ? "Edit URL" : "View Image"}
+                    onClick={() => setIsEditing((current) => !current)}
+                />
+            </ToolbarGroup>
+        </BlockControls>
+    );
+
     return (
         <>
-            <BlockControls>
-                <ToolbarGroup>
-                    <ToolbarButton
-                        icon={isEditing ? "edit" : "visibility"}
-                        label={isEditing ? "Edit URL" : "View Image"}
-                        onClick={() => setIsEditing((current) => !current)}
-                    />
-                </ToolbarGroup>
-            </BlockControls>
+            {youtubeId && renderToolbar()}
             <div {...useBlockProps()}>
                 {isEditing ? (
-                    <TextControl
-                        label="YouTube URL"
-                        value={url || ""}
-                        onChange={handleUrlChange}
-                        placeholder="Enter YouTube URL"
-                    />
+                    <div className="lazy-load-edit-wrapper">
+                        <div className="lazy-load__title">
+                            YouTube URL
+                        </div>
+
+                        <div className="lazy-load__title__instructions">
+                            Paste a link to the content you want to display on
+                            your site.
+                        </div>
+
+                        <TextControl
+                            value={url || ""}
+                            onChange={handleUrlChange}
+                            placeholder="Enter YouTube URL"
+                        />
+                    </div>
                 ) : youtubeId ? (
                     renderPreview()
-                ) : (
-                    <div className="empty-block-message">
-                        Video block is empty
-                    </div>
-                )}
+                ) : null}
             </div>
         </>
     );
