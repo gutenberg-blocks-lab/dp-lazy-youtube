dfcc83d87ba5fdcb0e0c6220d705f6b06b3ebe50
Marko Krstic <mailme@markokrstic.com>
2024-02-23 19:20:32 +0100

    1.0.8

--- dp-lazy-youtube.php
+++ dp-lazy-youtube.php
@@ -5,7 +5,7 @@
  * Description:       Block that load YouTube video scripts only if user clicks on the play button
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           1.0.7
+ * Version:           1.0.8
  * Author:            Marko Krstić
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -20,169 +20,37 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * For plugin to work on multiple installs replace 'UNIQUE_PLUGIN_NAME'
+ * Update plugin _VER on the line 32
+ * Update plugin _REMOTE_URL on the line 34
+ */
+$plugin_prefix = 'DPLAZYYOUTUBEBLOCK';
+
+define($plugin_prefix . '_DIR', plugin_basename(__DIR__));
+define($plugin_prefix . '_BASE', plugin_basename(__FILE__));
+define($plugin_prefix . '_PATH', plugin_dir_path(__FILE__));
+define($plugin_prefix . '_VER', '1.0.8');
+define($plugin_prefix . '_CACHE_KEY', 'dpyoutubeblock-cache-key-for-plugin');
+define($plugin_prefix . '_REMOTE_URL', 'https://selfhost.dplugins.com/wp-content/uploads/plugins/13/info.json');
+
+require constant($plugin_prefix . '_PATH') . 'inc/update.php';
+
+new DPUpdateChecker(
+	constant($plugin_prefix . '_DIR'),
+	constant($plugin_prefix . '_VER'),
+	constant($plugin_prefix . '_CACHE_KEY'),
+	constant($plugin_prefix . '_REMOTE_URL'),
+	constant($plugin_prefix . '_BASE')
+);
+
+/**
  * Registers the block using the metadata loaded from the `block.json` file.
  * Behind the scenes, it registers also all assets so they can be enqueued
  * through the block editor in the corresponding context.
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-// function dp_block_lazy_youtube() {
-// 	register_block_type( __DIR__ . '/build' );
-// }
-// add_action( 'init', 'dp_block_lazy_youtube' );
-
-
-if (!class_exists('mishaUpdateChecker')) {
-
-	class mishaUpdateChecker
-	{
-
-		public $plugin_slug;
-		public $version;
-		public $cache_key;
-		public $cache_allowed;
-
-		public function __construct()
-		{
-
-			$this->plugin_slug = plugin_basename(__DIR__);
-			$this->version = '1.0.7';
-			$this->cache_key = 'misha_custom_upd';
-			$this->cache_allowed = false;
-
-			add_filter('plugins_api', array($this, 'info'), 20, 3);
-			add_filter('site_transient_update_plugins', array($this, 'update'));
-			add_action('upgrader_process_complete', array($this, 'purge'), 10, 2);
-		}
-
-		public function request()
-		{
-
-			$remote = get_transient($this->cache_key);
-
-			if (false === $remote || !$this->cache_allowed) {
-
-				$remote = wp_remote_get(
-					'https://projectdev.cc/plugins/blocks-lazyload/info.json',
-					array(
-						'timeout' => 10,
-						'headers' => array(
-							'Accept' => 'application/json'
-						)
-					)
-				);
-
-				if (
-					is_wp_error($remote)
-					|| 200 !== wp_remote_retrieve_response_code($remote)
-					|| empty(wp_remote_retrieve_body($remote))
-				) {
-					return false;
-				}
-
-				set_transient($this->cache_key, $remote, DAY_IN_SECONDS);
-			}
-
-			$remote = json_decode(wp_remote_retrieve_body($remote));
-
-			return $remote;
-		}
-
-
-		function info($res, $action, $args)
-		{
-
-			// print_r( $action );
-			// print_r( $args );
-
-			// do nothing if you're not getting plugin information right now
-			if ('plugin_information' !== $action) {
-				return $res;
-			}
-
-			// do nothing if it is not our plugin
-			if ($this->plugin_slug !== $args->slug) {
-				return $res;
-			}
-
-			// get updates
-			$remote = $this->request();
-
-			if (!$remote) {
-				return $res;
-			}
-
-			$res = new stdClass();
-
-			$res->name = $remote->name;
-			$res->slug = $remote->slug;
-			$res->version = $remote->version;
-			$res->tested = $remote->tested;
-			$res->requires = $remote->requires;
-			$res->author = $remote->author;
-			$res->author_profile = $remote->author_profile;
-			$res->download_link = $remote->download_url;
-			$res->trunk = $remote->download_url;
-			$res->requires_php = $remote->requires_php;
-			$res->last_updated = $remote->last_updated;
-
-			$res->sections = array(
-				'description' => $remote->sections->description,
-				'installation' => $remote->sections->installation,
-				'changelog' => $remote->sections->changelog
-			);
-
-			if (!empty($remote->banners)) {
-				$res->banners = array(
-					'low' => $remote->banners->low,
-					'high' => $remote->banners->high
-				);
-			}
-
-			return $res;
-		}
-
-		public function update($transient)
-		{
-
-			if (empty($transient->checked)) {
-				return $transient;
-			}
-
-			$remote = $this->request();
-
-			if (
-				$remote
-				&& version_compare($this->version, $remote->version, '<')
-				&& version_compare($remote->requires, get_bloginfo('version'), '<=')
-				&& version_compare($remote->requires_php, PHP_VERSION, '<')
-			) {
-				$res = new stdClass();
-				$res->slug = $this->plugin_slug;
-				$res->plugin = plugin_basename(__FILE__); // misha-update-plugin/misha-update-plugin.php
-				$res->new_version = $remote->version;
-				$res->tested = $remote->tested;
-				$res->package = $remote->download_url;
-
-				$transient->response[$res->plugin] = $res;
-			}
-
-			return $transient;
-		}
-
-		public function purge($upgrader, $options)
-		{
-
-			if (
-				$this->cache_allowed
-				&& 'update' === $options['action']
-				&& 'plugin' === $options['type']
-			) {
-				// just clean the cache when new plugin version is installed
-				delete_transient($this->cache_key);
-			}
-		}
-	}
-
-	new mishaUpdateChecker();
+function dp_lazy_youtube_init() {
+	register_block_type( __DIR__ . '/build' );
 }
+add_action( 'init', 'dp_lazy_youtube_init' );
